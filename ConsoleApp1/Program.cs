e95a0405d0e33d4a847d0b78583d2c2b38838d99
ppavlov <petromilpavlov@gmail.com>
2019-08-10 18:36:16 +0300

    Add Example Project Files

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
+
+namespace ConsoleApp1
+{
+    public class Tesseract
+    {
+        public string Path { get; set; }
+
+        public string TesseractDll { get; set; }
+
+        public string LiptonicaDll { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile($"appsettings.json");
+
+            var configuration = builder.Build();
+
+            var tesseractConfig = configuration
+                .GetSection(nameof(Tesseract))
+                .Get<Tesseract>();
+
+
+        }
+    }
+}
---
+++ ConsoleApp1/build.ps1
@@ -0,0 +1,25 @@
+﻿Write-Output "Executing build.ps1";
+
+$appSettings = Get-Content 'appSettings.json' | Out-String | ConvertFrom-Json;
+
+$tesseractPath = $appSettings.Tesseract.Path;
+
+$tesseractExe = $appSettings.Tesseract.Executable;
+
+$tesseractDLL = $appSettings.Tesseract.TesseractDll;
+$liptonicaDLL = $appSettings.Tesseract.LiptonicaDll;
+
+$tesseractDLLPath = Join-Path $tesseractPath $tesseractDLL;
+$tesseractExePath = Join-Path $tesseractPath $tesseractExe;
+$liptonicaDLLPath = Join-Path $tesseractPath $liptonicaDLL;
+
+Write-Output "Copying $tesseractDLLPath to $tesseractDLL";
+Copy-Item -Path $tesseractDLLPath -Destination $tesseractDLL;
+
+Write-Output "Copying $liptonicaDLLPath to $liptonicaDLL";
+Copy-Item -Path $liptonicaDLLPath -Destination $liptonicaDLL;
+
+Write-Output "Copying $tesseractExePath to $tesseractExe";
+Copy-Item -Path $tesseractExePath -Destination $tesseractExe;
+
+Write-Output "Finished executing build.ps1";
